fe255fb810bd324dbcce7ac4780e0917443da1ed
MajdEwawi <177535@ppu.edu.ps>
2020-01-14 14:46:23 +0300

    bind the data to currrent day view

--- src/app/weather/currentdayweather/currentdayweather.component.ts
+++ src/app/weather/currentdayweather/currentdayweather.component.ts
@@ -11,12 +11,10 @@ import { Weather } from '../weather';
 export class CurrentdayweatherComponent implements OnInit {
   latitude : String = ' ';
   longitude :String = ' ';
+  location : Object ;
   weather : Weather;
   weatherList : Object [];
-  tempreture : number;
-  city : String;
-  minTemp : number;
-  maxTemp : number;
+
   constructor(private locationService : LocationService , private weatherService : WeatherService) {
    }
    
@@ -27,19 +25,16 @@ export class CurrentdayweatherComponent implements OnInit {
       console.log(data);
       this.latitude = data.latitude;
       this.longitude = data.longitude;
+      this.location =  data;
       this.weatherService.getWeatherForcast(this.latitude , this.longitude)
       .subscribe(data =>{   
         console.log(data);
         this.weather = data;
-        this.weatherList = this.weather.weatherList;
-         this.tempreture = Math.floor(this.weather.list[0].main.temp - 273);
-         this.minTemp = Math.floor(this.weather.list[0].main.temp_max - 273);
-         this.maxTemp = Math.floor(this.weather.list[0].main.temp_min - 273);
-         this.city = this.weather.city.name;
-        console.log(this.tempreture);
+
       }); 
     });
     console.log (this.weather);
   }
 
 }
+// 
