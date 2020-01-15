0a7b2961955a1a3ff86f411f0ce856600f9ed23f
majd ewawi <majd199ziad@gmail.com>
2020-01-15 19:22:37 +0200

    provide parameters to the sether detail icon

--- src/app/weather/otherdaysweather/otherdaysweather.component.ts
+++ src/app/weather/otherdaysweather/otherdaysweather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-otherdaysweather',
@@ -12,9 +13,13 @@ export class OtherdaysweatherComponent implements OnInit {
   @Input() icon;
   @Input() description;
   @Input() index;
-  constructor() { }
+  latitude : number;
+  longitude : number;
+  constructor(private actroute : ActivatedRoute) { }
 
   ngOnInit() {
+    this.latitude = this.actroute.snapshot.params.lat;
+    this.longitude = this.actroute.snapshot.params.lng;
  
   }
 
